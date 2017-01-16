083164efed186d004e6187a36920608a4235c487
Irene <carina.omsk@gmail.com>
2017-01-16 22:08:57 +0000

    Made a module

---
+++ http-functions.js
@@ -0,0 +1,22 @@
+var https = require('https');
+
+module.exports = function getHTML (options, callback) {
+    https.get(options, function (response) {
+
+    response.setEncoding('utf8');
+
+    var dataStr = '';
+
+    response.on('data', function (data) {
+      dataStr += data;
+    });
+
+    response.on('end', function () {
+      console.log('Response stream complete.');
+
+      // console.log(dataStr);
+      callback(dataStr);
+    });
+
+  });
+};

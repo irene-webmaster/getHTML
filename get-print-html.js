cbb183baf8b244911169dfcb56f32e186bc01483
Irene <carina.omsk@gmail.com>
2017-01-16 20:57:13 +0000

    Added data to a variable

--- get-print-html.js
+++ get-print-html.js
@@ -1,26 +1,29 @@
 var https = require('https');
 
-function getAndPrintHTMLChunks() {
+function getAndPrintHTML() {
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
   https.get(requestOptions, function (response) {
 
     response.setEncoding('utf8');
 
+    var dataStr = '';
+
     response.on('data', function (data) {
-      console.log('Chunk is:', data , '\n');
+      dataStr += data;
     });
 
     response.on('end', function() {
       console.log('Response stream complete.');
+      console.log(dataStr);
     });
 
   });
 
 }
 
-getAndPrintHTMLChunks();
+getAndPrintHTML();
